afd0a0a8b9ecc7f611b41349532a984e99e934e9
sunJungonly <sunnyoony@gmail.com>
2025-05-03 11:43:38 +0900

    uart_rx

---
+++ UART_RX_Peripheral/0502_RISC_V_UART_RX.srcs/sources_1/new/uart_tx.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2025/05/02 15:01:10
+// Design Name: 
+// Module Name: uart_tx
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module uart_tx(
+
+    );
+endmodule
